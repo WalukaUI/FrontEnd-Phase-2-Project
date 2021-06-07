96f5a6710a9379258c087696a4e806da80ea2b99
WalukaUI <cwaluka@yahoo.com>
2021-06-07 13:14:59 -0700

    Navbar completed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,35 @@
-function NavBar(){
-    return(
-<nav className="navbar navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">
-      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" className="d-inline-block align-text-top"/>
-      ABC Company
-    </a>
-  </div>
-</nav>
-    )
+import "./navBar.css"
+
+function NavBar() {
+  return (
+
+    <nav className="navbar navbar-light bg-light">
+      <div>
+        <a className="navbar-brand " href="#">
+          <img src="https://media.giphy.com/media/eM7h2gN2uvNVzzw1jA/giphy.gif" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+      
+        </a>
+      </div>
+
+      <div className="line1">
+        <input className="form-control mr-sm-2 " type="text" placeholder="Search" />
+      </div>
+      <div>
+      <select className="form-select" aria-label="Default select example">
+        <option selected>Select Type</option>
+        <option value="1">Electronic</option>
+        <option value="2">Foods</option>
+        <option value="3">Toys</option>
+      </select>
+      </div>
+      <div className="line2">
+        <button className="btn btn-success " type="submit">Search</button>
+      </div>
+
+    </nav>
+  )
 }
 
 export default NavBar
+
+//

6532a71c4e782acd7f42ff2b488feff6391f6663
WalukaUI <cwaluka@yahoo.com>
2021-06-08 22:05:18 -0700

    popup css completed

--- src/components/Products/Product/PopUp.js
+++ src/components/Products/Product/PopUp.js
@@ -1,18 +1,19 @@
 import "../Product/popup.css"
 
 function PopUp(props) {
+    
     return (props.trigger) ? (
         <div className="popup-box" >
             <div className="popup-inner">
                 <div className="formDiv div1">
                     <form >
-                        <h4>Add a New Product</h4>
+                        <h4>Update Product</h4>
                         <div className="form-group row">
                             <label >Product Name<input name="product_name" className="form-control form-control-sm" placeholder="Name" /></label>
                             <label >Price<input name="product_price" type="number" className="form-control form-control-sm" placeholder="Price" /></label>
                             <label >Description<input name="products_coo" className="form-control form-control-sm" placeholder="Description" /></label>
                             <label >Category
-                <select className="form-select" name="product_master_category" aria-label="Default select example" >
+                              <select className="form-select" name="product_master_category" aria-label="Default select example" >
                                     <option value="331">Electronic</option>
                                     <option value="333">Toys</option>
                                     <option value="334">Computer Parts</option>
@@ -24,13 +25,12 @@ function PopUp(props) {
                             <div className="container">
                                 <div className="row">
                                     <div className="col-sm">
-                                    <button className=" btn btn-primary" onClick={() => props.togglePopup()}>Update Product</button>
+                                    <button className=" btn btn-success" onClick={() => props.togglePopup()}>Update Product</button>
                                     </div>
                                     <div className="col-sm">
-                                    <button className="btn btn-primary" type="submit">Create Product</button>
+                                    <button className="btn btn-danger" type="submit">Cancel</button>
                                     </div>
-                                    <div className="col-sm">
-                                        
+                                    <div className="col-sm"> 
                                     </div>
                                 </div>
                             </div>

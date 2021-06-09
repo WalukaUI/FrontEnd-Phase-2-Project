2f10587858a605e0a7908a84d003d4116828c37b
WalukaUI <cwaluka@yahoo.com>
2021-06-08 21:40:39 -0700

    added basic form to popup

--- src/components/Products/Product/PopUp.js
+++ src/components/Products/Product/PopUp.js
@@ -5,8 +5,29 @@ function PopUp(props) {
         <div className="popup-box" >
             <div className="popup-inner">
                 <button className=" btn btn-primary" onClick={()=>props.togglePopup()}>Update Product</button>
+                <div className="formDiv div1">
+        <form >
+            <h4>Add a New Product</h4>
+            <div className="form-group row">
+                <label >Product Name<input name="product_name"  className="form-control form-control-sm" placeholder="Name"  /></label>
+                <label >Price<input name="product_price"  type="number" className="form-control form-control-sm" placeholder="Price"  /></label>
+                <label >Description<input name="products_coo"  className="form-control form-control-sm" placeholder="Description"  /></label>
+                <label >Category
+                <select className="form-select" name="product_master_category"  aria-label="Default select example" >
+                        <option value="331">Electronic</option>
+                        <option value="333">Toys</option>
+                        <option value="334">Computer Parts</option>
+                        <option value="332">Other</option>
+                    </select>
+                </label>
+                <label>ImageURL<input name="product_image"  className="form-control form-control-sm" placeholder="ImageURL"  /></label>
+                <label>Quantity<input name="product_stock"  type="number" className="form-control form-control-sm" placeholder="Quantity"  /></label>
+
+                <button className="btn btn-primary btnForm" type="submit">Create Product</button>
             </div>
-          <h1>popup awaaa</h1>
+        </form>
+    </div>
+            </div>
         </div>
     ) : "";
 }

fa05e88ddddefc1276b50de62ff4b98ce9bd9405
WalukaUI <cwaluka@yahoo.com>
2021-06-13 00:19:43 -0700

    display category styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,18 +14,18 @@ function cardsByCategory(e){
     <nav className="navbar navbar-light bg-light">
       <div >
         <a className="navbar-brand " href="#">
-          <img src="https://media.giphy.com/media/eM7h2gN2uvNVzzw1jA/giphy.gif" alt="" width="30" height="24" className="d-inline-block align-text-top" />
+          <img src="https://media.giphy.com/media/eM7h2gN2uvNVzzw1jA/giphy.gif" alt="logo"  className="logo" />
 
         </a>
       </div>
-       <button onClick={()=>setviewChanger(true)}>List View</button>
-       <button onClick={()=>setviewChanger(false)}>Grid View</button>
+       <button onClick={()=>setviewChanger(true)} className="btn btn-info">List View</button>
+       <button onClick={()=>setviewChanger(false)}className="btn btn-info">Grid View</button>
       <div className="line1">
-        <input className="form-control mr-sm-2 " type="text" placeholder="Search" onChange={activeSearch}/>
+        <input className="form-control mr-sm-2 " type="text" placeholder="Search by Name" onChange={activeSearch}/>
       </div>
       <div className="line2">
         <p>Display Category </p>
-        <select className="form-select" aria-label="Default select example" onChange={cardsByCategory}>
+        <select className="form-select selector" aria-label="Default select example" onChange={cardsByCategory}>
         <option value="All">All</option>
           <option value="331">Electronic</option>
           <option value="333">Toys</option>

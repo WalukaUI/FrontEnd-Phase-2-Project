15a0c7f2523c46fa0b9c70be5cded3d48cfdd52c
WalukaUI <cwaluka@yahoo.com>
2021-06-14 11:17:59 -0700

    add delete limit to 20

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import "./singleProduct.css"
 
-//const URL="https://phase-ii-inventory-database.herokuapp.com/products"
-const URL="http://localhost:3000/products"
+const URL="https://phase-ii-inventory-database.herokuapp.com/products"
+//const URL="http://localhost:3000/products"
 
 function ProductDetail() {
   const [singleProduct, setsingleProduct] = useState(null);

98a5c18217c6f0908d5fe4b71193b0d4c7a5a87d
WalukaUI <cwaluka@yahoo.com>
2021-06-14 11:16:24 -0700

    put delete limit to 20

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -13,8 +13,8 @@ function Main() {
   const [serachCatergory, setSearchCatergory] = useState("All")
   const [viewChanger, setviewChanger] = useState(true)
 
-  const URL="https://phase-ii-inventory-database.herokuapp.com/products"
-  //const URL = "http://localhost:3000/products"
+  //const URL="https://phase-ii-inventory-database.herokuapp.com/products"
+  const URL = "http://localhost:3000/products"
 
   useEffect(() => {
     let getrequestOptions = {
@@ -48,16 +48,22 @@ function Main() {
   }
 
   function removefromStock(item) {
+
     const postrequestOptions = {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
     }
-    fetch(`${URL}/${item}`, postrequestOptions)
-      .then(r => r.json())
-      .then(j => {
-        let newProductList = product.filter((x) => x.id !== item)
-        setProduct(newProductList)
-      })
+
+    if (product.length > 20) {
+      fetch(`${URL}/${item}`, postrequestOptions)
+        .then(r => r.json())
+        .then(j => {
+          let newProductList = product.filter((x) => x.id !== item)
+          setProduct(newProductList)
+        })
+    } else {
+      alert("Reached minimum limit of products, Cannot Delete")
+    }
   }
   function markasOutofStock(item) {
     let produtAvilability = item.isInMarket
@@ -95,43 +101,43 @@ function Main() {
   }
 
   return (
-  <main>
-    <div className="app" style={{
-      backgroundImage: `url(${image})`
-    }}>
-      <NavBar
-        searchInDataBase={setSearchtearm}
-        setSearchCatergory={setSearchCatergory}
-        setviewChanger={setviewChanger}
-      />
-     <div className="cont">
-      <div className="row">
-        <div className="col-lg-8 col-md-12">
-          <Switch>
-            <Route path="/products/:id">
-              <ProductDetail />
-            </Route>
-            <Route path="/">
-              <AllProducts
-                product={product}
-                removefromStock={removefromStock}
-                changeAvailability={markasOutofStock}
-                updateOldProduct={updateOldProduct}
-                serachTerm={serachTerm}
-                serachCatergory={serachCatergory}
-                viewChanger={viewChanger}
-              />
-            </Route>
-          </Switch>
+    <main>
+      <div className="app" style={{
+        backgroundImage: `url(${image})`
+      }}>
+        <NavBar
+          searchInDataBase={setSearchtearm}
+          setSearchCatergory={setSearchCatergory}
+          setviewChanger={setviewChanger}
+        />
+        <div className="cont">
+          <div className="row">
+            <div className="col-lg-8 col-md-12">
+              <Switch>
+                <Route path="/products/:id">
+                  <ProductDetail />
+                </Route>
+                <Route path="/">
+                  <AllProducts
+                    product={product}
+                    removefromStock={removefromStock}
+                    changeAvailability={markasOutofStock}
+                    updateOldProduct={updateOldProduct}
+                    serachTerm={serachTerm}
+                    serachCatergory={serachCatergory}
+                    viewChanger={viewChanger}
+                  />
+                </Route>
+              </Switch>
 
-        </div>
-        <div className="col-lg-4 col-md-12">
-          <Form createNewProduct={createNewProduct} />
+            </div>
+            <div className="col-lg-4 col-md-12">
+              <Form createNewProduct={createNewProduct} />
+            </div>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
-  </main>
+    </main>
   )
 }
 
